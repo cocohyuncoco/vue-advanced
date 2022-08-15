d55904dd93cd3fade1d56f80ec102d87d168f9ea
cocohyuncoco <guswjd8287@gmail.com>
2022-08-15 16:12:04 +0900

    [임현정] <컴포넌트 디자인 패턴> Slot - 마크업 확장이 가능한 컴포넌트

--- design/slots/src/App.vue
+++ design/slots/src/App.vue
@@ -1,28 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <ul>
+      <!-- 기존 컴포넌트는 이런 모양 <app-header></app-header> -->
+
+      <!-- slot은 props 안내려도 v-for로 데이터 출력 가능  -->
+      <item v-for="(item, idx) in items" v-bind:key="idx">{{ item }}</item>
+
+      <item>
+        <!-- slot의 장점 -->
+        <!-- 만약 요구사항이 바뀔때 유연하게 대처 가능, 모델 마음대로 확장 가능 -->
+        아이템 1
+      </item>
+
+      <item>
+        <!-- 2. 버튼을 넣어주세요 -->
+        아이템 2
+        <button>click me</button>
+      </item>
+
+      <item>
+        <!-- 3. 텍스트와 함께 이미지를 표현해주세요 -->
+        <div>
+          아이템 3
+        </div>
+        <img src="./assets/f7.png" alt="">
+      </item>
+
+      <item>
+        <!-- 4. slot에는 style도 넣을수 있단 -->
+        <div style="color: blue; font-size: 20px;">
+          아이템 4
+        </div>
+      </item>
+
+      <!-- <item v-for="(item, idx) in items" v-bind:key="idx" :item="item"></item> -->
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Item from './Item.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+ components:{
+    Item,
+ },
+ data() {
+  return {
+    items: ['slot 1', 'slot 2', 'slot 3', 'slot 4', 'slot 5']
+  };
+ },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+
 </style>
---
+++ design/slots/src/Item.vue
@@ -0,0 +1,16 @@
+<template>
+    <li>
+        <slot>
+            <!-- 등록하는 곳에서 정의할 화면 영역 -->
+            <!-- {{ item }} -->
+        </slot>
+    </li>
+</template>
+
+
+<script>
+export default{
+    // props: ['item']
+}
+
+</script>
