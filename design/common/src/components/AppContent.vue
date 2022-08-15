cd1d49f3440cbadc7379f8db1b5e9975a3b0ba3b
cocohyuncoco <guswjd8287@gmail.com>
2022-08-15 15:14:26 +0900

    [임현정] <컴포넌트 디자인 패턴> Common - 기본적인 컴포넌트 등록과 컴포넌트 통신

---
+++ design/common/src/components/AppContent.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(item, idx) in items" v-bind:key="idx">
+                {{item}}
+            </li>
+        </ul>
+
+        <!-- 메서드 안만들고 클릭하자마자 상위 컴포넌트로 이벤트 올려보내기 가능 -->
+        <button @click="$emit('renew')">renew items</button>
+    </div>
+</template>
+
+<script>
+export default {
+
+  // Props Validation
+  // array 가 아닌 데이터가 들어오면 에러처리
+  props:{
+    items:{
+        type: Array,
+        required: true,
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ design/common/src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+    // props: ['title']
+
+    // props 벨리데이션 
+    props:{
+        title: String,
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
