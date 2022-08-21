a695531f2ace46324ed3f74e1c798d639dc01c50
cocohyuncoco <guswjd8287@gmail.com>
2022-08-21 15:41:39 +0900

    [임현정] <컴포넌트 디자인 패턴> controlled - 하위에서 관리해야 했던 데이터를 상위에서 관리하게 하는 방법

---
+++ design/controlled/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- <check-box :checked="checked"></check-box> -->
+  <div>
+    <check-box v-model="checked"></check-box>
+  </div>
+</template>
+<!-- <script src="http://code.jquery.com/jquery-latest.min.js"></script> -->
+
+<script>
+import CheckBox from './components/CheckBox.vue'
+
+export default {
+  // @input 이벤트
+  // :value 값
+  components:{
+    CheckBox,
+  },
+  data(){
+    return{
+      checked: false,
+    }
+  },  
+};
+</script>
+
+<style scoped>
+
+
+</style>
---
+++ design/controlled/src/components/CheckBox.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <!-- <input type="checkbox" v-model="checked"> -->
+        <input type="checkbox" :value="value" @click="toggleCheckBox"></input>
+    </div>
+</template>
+
+<script>
+export default {
+    // props: ['checked'],
+    props: ['value'],
+    methods: {
+        toggleCheckBox() {
+            this.$emit('input', !this.value) // true로 올리는것보단 반대로 되야하니까 not! 
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
